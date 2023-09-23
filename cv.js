7fa59c870706a312250c02a573e66265f75718d7
Pay2023Arg <145825940+Pay2023Arg@users.noreply.github.com>
2023-09-23 03:23:57 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,8 @@
+document.getElementById('botonhobby').addEventListener('click',function() {
+    location. href = 'hobby.html' 
+}
+)
+document.getElementById('botonexp').addEventListener('click',function() {
+    location. href = 'Experiencia.html' 
+}
+)
